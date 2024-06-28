d0d017f01404509329dd63c31fb9e1bb4b78c935
Eduardo Soares <eduardo@pladoo.com.br>
2024-06-27 23:31:36 -0300

    Update S3Data.js

--- Data/S3Data.js
+++ Data/S3Data.js
@@ -1,7 +1,7 @@
-const s3_bucket = 'myhotspace';
+const s3_bucket = 'xxxxxxxxx';
 const s3_region = 'us-east-1';
-const s3_accessKey = 'AKIA5XVIABF6VHBW2Q3N';
-const s3_secretKey = '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';
+const s3_accessKey = 'XXXXXXXXXXXXXXX';
+const s3_secretKey = 'XXXXXXXXXXXXXXXXXXXXXXXXX';
 
 const s3_config = {
     region: s3_region,
